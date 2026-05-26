538a05a0eab47614a9ab8dd796318dedffe65004
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2026-05-26 12:29:00 +0100

    Repackage K2 jailbreak

---
+++ build_k2.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+echo "==========================="
+echo "=  K2 Jailbreak Builder   ="
+echo "=  Created by Hackerdude  ="
+echo "= (Based on NiLuJe's JB)  ="
+echo "==========================="
+
+rm -rf build_tmp/k2
+rm -rf build/k2
+mkdir -p build_tmp/k2
+mkdir -p build/k2
+
+echo "[*] Building update-adds payload..."
+tar -czf ./build_tmp/k2/update-adds.tar.gz \
+    --transform='flags=r;s|src/payload/root_link|root|' \
+    --transform='flags=r;s|src/payload/||' \
+    --owner=root --group=root \
+    src/payload/root_link \
+    src/payload/root/etc/uks/pubhackkey01.pem \
+    src/payload/root/var/local/java/keystore/developer.keystore \
+    src/payload/root/opt/amazon/ebook/lib/json_simple-1.1.jar
+
+echo "[*] Copying files..."
+cp -ra src/k2/* build_tmp/k2/
+
+echo "[*] Building JB update files"
+KINDLE_MODELS="k2 k2i dx dxi dxg"
+for model in ${KINDLE_MODELS} ; do
+	# Prepare our files for this specific kindle model...
+	ARCH=${model}
+
+	# Build install update
+	tar --hard-dereference \
+        --owner root --group root \
+        -cvzf build_tmp/k2/${ARCH}.tgz \
+        --transform='flags=r;s|build_tmp/k2/||' \
+        --transform="flags=r;s|2.5-jailbreak.dat|update_${ARCH}.dat|" \
+        build_tmp/k2/*
+
+    kindletool create ota -d ${model} build_tmp/k2/${ARCH}.tgz build/k2/Update_${ARCH}_install.bin
+    kindletool create ota -d ${model} src/k2/uninstall/uninstall.sh build/k2/Update_${ARCH}_uninstall.bin
+done
---
+++ src/k3_3.0-3.2/jailbreak_0.13.N_k3g-3.0-to-3.2.sig
@@ -0,0 +1,1 @@
+129 cf35d2ca1b76edf936fabdd2a105ce86 3.1-jb.sig 148 3.1-jb
